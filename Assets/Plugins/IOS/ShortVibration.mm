f526292887edfac212cec47b5feef0e484243221
Sinan Cem Cayli <sinancem@sabanciuniv.edu>
2019-10-30 04:00:18 +0300

    fix to vibration name

--- Assets/Plugins/IOS/ShortVibration.mm
+++ Assets/Plugins/IOS/ShortVibration.mm
@@ -10,27 +10,41 @@ extern "C"
 		
         switch (type) {
         case FeedbackType_Selection:
+		{
             UInt32 pop = SystemSoundID(1520);
             AudioServicesPlaySystemSound(pop);
             break;
+		}
         case FeedbackType_Impact_Light:
-            [HapticHelper generateFeedback:FeedbackType_Impact_Light];
+		{
+            [HapticInterface generateFeedback:FeedbackType_Impact_Light];
             break;
+		}
         case FeedbackType_Impact_Medium:
-            [HapticHelper generateFeedback:FeedbackType_Impact_Medium];
+		{
+            [HapticInterface generateFeedback:FeedbackType_Impact_Medium];
             break;
+		}
         case FeedbackType_Impact_Heavy:
-            [HapticHelper generateFeedback:FeedbackType_Impact_Heavy];
+		{
+            [HapticInterface generateFeedback:FeedbackType_Impact_Heavy];
             break;
+		}
         case FeedbackType_Notification_Success:
-            [HapticHelper generateFeedback:FeedbackType_Notification_Success];
+		{
+            [HapticInterface generateFeedback:FeedbackType_Notification_Success];
             break;
+		}
         case FeedbackType_Notification_Warning:
-            [HapticHelper generateFeedback:FeedbackType_Notification_Warning];
+		{
+            [HapticInterface generateFeedback:FeedbackType_Notification_Warning];
             break;
+		}
         case FeedbackType_Notification_Error:
-            [HapticHelper generateFeedback:FeedbackType_Notification_Error];
+		{
+            [HapticInterface generateFeedback:FeedbackType_Notification_Error];
             break;
+		}
         default:
             break;
 		}
